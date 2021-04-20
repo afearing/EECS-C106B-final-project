20d15da246db86054718d55dba667ec2a69475bc
afearing <afearing@berkeley.edu>
2021-04-19 21:43:56 -0700

    started object-oriented implementation

--- stda-simulator/beauty/stda_sim_beautiful.py
+++ stda-simulator/beauty/stda_sim_beautiful.py
@@ -5,13 +5,48 @@ import yaml
 import io
 import math
 
-"""
-notes on implementation
-STATE = [x_pos, y_pos, z_pos, roll, pitch, yaw, x_vel, y_vel, z_vel, roll_rate, pitch_rate, yaw_rate, rudder_angle, sail_angle] in R^14. All SI units, radians
+
+# notes on implementation
+# STATE = [x_pos, y_pos, z_pos, roll, pitch, yaw, x_vel, y_vel, z_vel, roll_rate, pitch_rate, yaw_rate, rudder_angle, sail_angle] in R^14. All SI units, radians
 
 
 
-"""
+
+
+class Boat:
+    """The class for the boat state"""
+    def __init__(self, boat_params):
+        # self.pos_x   = initial_state['pos_x']
+        # self.pos_y   = initial_state['pos_y'] 
+        # self.pos_z   = initial_state['pos_z']
+        # self.vel_x   = initial_state['vel_x']
+        # self.vel_y   = initial_state['vel_y']
+        # self.vel_z   = initial_state['vel_z']
+        # self.roll    = initial_state['roll']
+        # self.pitch   = initial_state['pitch']
+        # self.yaw     = initial_state['yaw']
+        # self.roll_rate = initial_state['roll_rate']
+        # self.pitch_rate = initial_state['pitch_rate']
+        # self.yaw_rate = initial_state['yaw_rate']
+        # self.sail_angle = initial_state['sail_angle']
+        # self.rudder_angle = initial_state['rudder_angle']
+        # self.self.boat_props = boat_params # leave it like this for the moment because idk what to do with the dimensions
+        for key, val in boat_params['initial_state'].items():
+            exec('self.' + key + '= val') # this is supposed to load all the parameters as variables https://stackoverflow.com/questions/18090672/convert-dictionary-entries-into-variables-python
+        for key, val in boat_params['dimensions'].items():
+            exec('self.' + key + '= val')
+
+
+    def calculate_speed(self):
+        return math.sqrt(self.vel_x**2 + self.vel_y**2)
+
+
+class Environment:
+    def __init__(self, env_params):
+        for key, val in env_params['properties'].items():
+            exec('self.' + key + '= val')
+        for key, val in env_params['disturbances'].items():
+            exec('self.' + key + '= val')
 
 def main():
     # simulation parameters. Set them here. We can change it to a yaml file to load later.
@@ -19,16 +54,15 @@ def main():
     with open('sim_config.yaml', 'r') as stream: #idek what this code does but it works https://stackoverflow.com/questions/1773805/how-can-i-parse-a-yaml-file-in-python
         sim_params = yaml.safe_load(stream)
     # run the simulation
-    print(sim_params, save = save)
-    simulate(sim_params, save = save)
-
-
-
-def simulate(sim_params, save = False):
     Ts = sim_params['simulator']['stepper']['stepsize']
-    boat_state = np.array(sim_params['initial_state'].values())
-    environment_parameters = sim_params['environment']
+    boat_state = Boat(sim_params['boat']) #np.array(sim_params['initial_state'].values())
+    environment_state = Environment(sim_params['environment'])
+    simulate(boat_state, environment_state, save = save)
 
+
+
+def simulate(boat_state, environment_state, save = False):
+    
 
     return False
 
@@ -36,13 +70,6 @@ def simulate(sim_params, save = False):
 
 
 def solve(time, boat_state, boat_ref, environment_parameters):
-    # State unpacking
-    pos_x,     pos_y,      pos_z    = boat_state[0        : 3]
-    roll,      pitch,      yaw      = boat_state[3        : 6]
-    vel_x,     vel_y,      vel_z    = boat_state[6        : 9]
-    roll_rate, pitch_rate, yaw_rate = boat_state[9        : 12]
-    rudder_angle, sail_angle        = boat_state[12       : 15]
-    speed = math.sqrt(vel_x**2 + vel_y**2)
 
     # For force calulations needed values
     speed = calculate_speed(boat_state)
@@ -69,18 +96,40 @@ def calculate_wave_influence(time, boat_state, environment_parameters):
     wave_direction =    environment_parameters['disturbances']['wave_direction']
     wave_amplitude =    environment_parameters['disturbances']['wave_amplitude']
     frequency = math.sqrt((2 * math.pi * g) / wave_length)
-
+    pos_x = boat_state[0]
+    pos_y = boat_state[1]
+    yaw   = boat_state[5]
     # wave vector components in cartesian coordinates
     wave_x = 2*math.pi / wave_length * math.cos(wave_direction)
     wave_y = 2*math.pi / wave_length * math.sin(wave_direction)
-    factor = -wave_amplitude * math.cos(frequency * time - wave_x * boat_state[0] = wave_y * boat_state[1])
+    factor = -wave_amplitude * math.cos(frequency * time - wave_x * pos_x - wave_y * pos_y)
     gradient_x = wave_x * factor
     gradient_y = wave_y * factor
 
-    height = wave_amplitude * math.sin(frequency * time - wave_x * boat_state[0] = wave_y * boat_state[1])
-    gradient_x = gradient_x * math.cos(boat_state[2]) + gradient_y * math.sin(boat_state[2])
-    gradient_y = gradient_y * math.cos(boat_state[2]) - gradient_x * math.sin(boat_state[2])
+    height = wave_amplitude * math.sin(frequency * time - wave_x * pos_x - wave_y * pos_y)
+    gradient_x = gradient_x * math.cos(yaw) + gradient_y * math.sin(yaw)
+    gradient_y = gradient_y * math.cos(yaw) - gradient_x * math.sin(yaw)
     return (height, gradient_x, gradient_y)
+
+def calculate_apparent_wind(boat_state, environment_parameters):
+    ''' Calculate the apparent wind on the boat. 
+    param true_wind:    The true wind directions
+
+    return: The apparent wind on the boat
+    '''
+    true_wind
+    transformed_x = true_wind.x * cos(yaw) + true_wind.y * sin(yaw)
+    transformed_y = true_wind.x * -sin(yaw) + true_wind.y * cos(yaw)
+
+    apparent_x = transformed_x - vel_x
+    apparent_y = transformed_y - vel_y
+    apparent_angle = atan2(-apparent_y, -apparent_x)
+    apparent_speed = sqrt(apparent_x**2 + apparent_y**2)
+
+    return ApparentWind(x=apparent_x,
+                        y=apparent_y,
+                        angle=apparent_angle,
+                        speed=apparent_speed)
 
 
 
