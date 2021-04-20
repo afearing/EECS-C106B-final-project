6f3d2890e490d839821484718689a1ae6a3b5fc5
afearing <afearing@berkeley.edu>
2021-04-19 22:09:41 -0700

    loaded invariants into environment

--- stda-simulator/beauty/stda_sim_beautiful.py
+++ stda-simulator/beauty/stda_sim_beautiful.py
@@ -18,20 +18,35 @@ class Boat:
     def __init__(self, boat_params):
         for key, val in boat_params['initial_state'].items():
             exec('self.' + key + '= val') # this is supposed to load all the parameters as variables https://stackoverflow.com/questions/18090672/convert-dictionary-entries-into-variables-python
-        for key, val in boat_params['dimensions'].items():
-            exec('self.' + key + '= val')
-
-
     def calculate_speed(self):
         return math.sqrt(self.vel_x**2 + self.vel_y**2)
 
 
 class Environment:
+    """The class for the environment and its properties. Note that this includes the boat dimensions"""
     def __init__(self, env_params):
         for key, val in env_params['properties'].items():
             exec('self.' + key + '= val')
         for key, val in env_params['disturbances'].items():
             exec('self.' + key + '= val')
+        for key, val in env_params['boat_dimensions'].items():
+            exec('self.' + key + '= val')
+
+        # Invariants
+        self.wave_impedance_invariant = (self.water_density / 2) * self.lateral_area
+        # Hydrostatic force
+        self.hydrostatic_eff_x        = self.height_bouyancy + (self.water_density / self.mass) * self.geometrical_moi_x
+        self.hydrostatic_eff_y        = self.height_bouyancy + (self.water_density / self.mass) * self.geometrical_moi_y
+        self.hydrostatic_invariant_z  =  -self.water_density * self.waterline_area * self.gravity
+        self.gravity_force            = self.mass * self.gravity
+        # Damping
+        self.damping_invariant_x      = -self.mass / self.along_damping
+        self.damping_invariant_y      = -self.mass / self.transverse_damping
+        self.damping_invariant_z      = -.5 * self.damping_z * math.sqrt(self.water_density * self.waterline_area * self.gravity * self.mass)
+        self.damping_invariant_yaw    = -(self.moi_z / self.yaw_timeconstant)
+        self.damping_invariant_pitch  = -2 * self.pitch_damping * math.sqrt(self.moi_y * self.mass * self.gravity * self.hydrostatic_eff_y)
+        self.damping_invariant_roll   = -2 * self.roll_damping * math.sqrt(self.moi_x * self.mass * self.gravity * self.hydrostatic_eff_x)
+        
         self.time = 0
         self.wind_x = self.wind_strength * math.cos(self.wind_direction)
         self.wind_y = self.wind_strength * math.sin(self.wind_direction)
@@ -63,14 +78,8 @@ def solve(boat_state, boat_ref, environment_parameters):
     speed = calculate_speed(boat_state)
     wave_influence = calculate_wave_influence(time, boat_state, environment_parameters)
     apparent_wind = calculate_apparent_wind(boat_state, environment_parameters)
-
-
-
-
-
-def calculate_speed(boat_state):
-    return np.linalg.norm(boat_state[3:5]) # sqrt(sumsq(x-y velocity))
-
+    
+    # Force calculation
 
 
 def calculate_wave_influence(boat, env):
@@ -110,6 +119,21 @@ def calculate_apparent_wind(boat, env):
     apparent_speed = math.sqrt(apparent_x**2 + apparent_y**2)
 
     return (apparent_x, apparent_y, angle=apparent_angle, speed=apparent_speed)
+
+def calculate_damping(boat, env):
+    ''' Calculate the damping. 
+    
+    return: The amount of damping applied to the boat
+    '''
+    return Damping(
+        x=DAMPING_INVARIANT_X * vel_x,
+        y=DAMPING_INVARIANT_Y * vel_y,
+        z=DAMPING_INVARIANT_Z * vel_z,
+        roll=DAMPING_INVARIANT_ROLL * roll_rate,
+        pitch=DAMPING_INVARIANT_PITCH * pitch_rate,
+        yaw=DAMPING_INVARIANT_YAW * yaw_rate)
+
+
 
 
 
