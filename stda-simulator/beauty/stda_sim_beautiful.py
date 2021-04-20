c02686e7793c66d3a223e53c6f1c6c24a525198d
afearing <afearing@berkeley.edu>
2021-04-19 19:06:17 -0700

    made initial states in config

--- stda-simulator/beauty/stda_sim_beautiful.py
+++ stda-simulator/beauty/stda_sim_beautiful.py
@@ -5,17 +5,20 @@ import yaml
 import io
 
 
+"""
+notes on implementation
+STATE = [x_pos, y_pos, z_pos, roll, pitch, yaw, x_vel, y_vel, z_vel, roll_rate, pitch_rate, yaw_rate, rudder, sail] in R^14. All SI units, radians
 
 
 
-
+"""
 def main():
     # simulation parameters. Set them here. We can change it to a yaml file to load later.
     save = True
     with open('sim_config.yaml', 'r') as stream: #idek what this code does but it works https://stackoverflow.com/questions/1773805/how-can-i-parse-a-yaml-file-in-python
         sim_params = yaml.safe_load(stream)
     # run the simulation
-    print(sim_params)
+    print(sim_params, save = save)
 
 
 
