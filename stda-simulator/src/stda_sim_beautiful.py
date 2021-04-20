8b115d27b9ae8125e5b1ff4d91b5d9ddec12be72
afearing <afearing@berkeley.edu>
2021-04-19 18:46:53 -0700

    added some python config to debugger

--- stda-simulator/src/stda_sim_beautiful.py
+++ stda-simulator/src/stda_sim_beautiful.py
@@ -12,7 +12,7 @@ import io
 def main():
     # simulation parameters. Set them here. We can change it to a yaml file to load later.
     save = True
-    with open('sim_params_config.yaml', 'r') as stream:
+    with open('sim_params_config.yaml', 'r') as stream: #idek what this code does but it works https://stackoverflow.com/questions/1773805/how-can-i-parse-a-yaml-file-in-python
         sim_params = yaml.safe_load(stream)
     # run the simulation
     print(sim_params)
